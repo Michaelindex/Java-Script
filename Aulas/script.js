3c9e455fd795978e19a469c65d5f592fcd899793
Michael Santos <mcialbr123@gmail.com>
2023-06-22 11:17:34 -0300

    form

---
+++ Aulas/script.js
@@ -0,0 +1,10 @@
+var slCidade = document.getElementById('slCidade')
+var pCidade = document.getElementById('cidade')
+var pCeu = document.getElementById('ceu')
+var pTemperatura = document.getElementById('temperatura')
+var pTempMin = document.getElementById('tempMin')
+var pTempMax = document.getElementById('tempMax')
+
+function obterDados(){
+    console.log(slCidade.value)
+}
